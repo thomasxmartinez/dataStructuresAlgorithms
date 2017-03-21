2c19d01af41fc89a9c729da410478e9e8a62a33b
tommytnez <tommytnez@yahoo.com>
2017-03-21 13:30:18 -0700

    added a few:

---
+++ factorialize.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function factorialize(num) {
+  if (num < 0)
+        return -1;
+  else if (num == 0)
+      return 1;
+  else {
+      return (num * factorialize(num - 1));
+  }
+}
+factorialize(5);
+
+function factorialize(num) {
+  var result = num;
+  if (num === 0 || num === 1)
+    return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
+}
+factorialize(5);
+
+function factorialize(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
+factorialize(5);
---
+++ reverseString.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+reverseString("hello");
+
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+reverseString('hello');
+
+function reverseString(str) {
+  if (str === "")
+    return "";
+  else
+    return reverseString(str.substr(1)) + str.charAt(0);
+}
+reverseString("hello");
