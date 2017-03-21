abe75106952a6e07cfbb035ac827817d4ab4e000
tommytnez <tommytnez@yahoo.com>
2017-03-21 15:05:44 -0700

    palindromes

---
+++ palindrome.js
@@ -0,0 +1,32 @@
+'use strict';
+
+function palindrome(str) {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama");
+
+function palindrome(str) {
+  var re = /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama");
+
+function palindrome(str) {
+  var re= /[\W_]/g;
+  var lowRegStr = str.toLowerCase().replace(re, '');
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama");
+
+function palindrome(str) {
+  var re= /[W_]/g;
+  var reverseStr = lowRegStr.split('').reverse().join('');
+  return reverseStr === lowRegStr;
+}
+palindrome("A man, a plan, a canal. Panama");
