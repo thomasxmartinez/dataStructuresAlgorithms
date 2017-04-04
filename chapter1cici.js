00395c5cbf593a31f3f3decbac6aa635978cf164
tommytnez <tommytnez@yahoo.com>
2017-04-04 14:03:49 -0700

    added palindrome

--- chapter1cici.js
+++ chapter1cici.js
@@ -179,3 +179,25 @@ function compressStr(str){
 
 
 compressStr('abcd');
+
+
+function palindromePermutation(str){
+      var distinct = 0;
+      var s_array = Array.apply(null, Array(256)).map(Number.prototype.valueOf, 0);
+      str = str.toLowerCase();
+      for(var i = 0; i < str.length; i++){
+        if(str[i] == ' '){
+          continue;
+        }
+        s_array[str[i].charCodeAt(0)]++;
+        if(s_array[str[i].charCodeAt(0)] % 2){
+          distinct++;
+        }else{
+          distinct--;
+        }
+      }
+      return (distinct < 2);
+    }
+
+  palindromePermutation('budunk')
+  
