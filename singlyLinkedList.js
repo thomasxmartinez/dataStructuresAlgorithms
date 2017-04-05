d28c366372ca80d7f6c7e2ef58d6cb08dadabe70
tommytnez <tommytnez@yahoo.com>
2017-04-05 14:49:44 -0700

    doubly linked list

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -81,3 +81,40 @@ singlyLinkedList.prototype.print = function(){
   }
   console.log(string.trim());
 };
+
+
+var singlyLinkedList = new SinglyLinkedList();
+singlyLinkedList.print(); // => ''
+singlyLinkedList.add(1);
+singlyLinkedList.add(2);
+singlyLinkedList.add(3);
+singlyLinkedList.add(4);
+singlyLinkedList.print(); // => 1 2 3 4
+console.log('length is 4:', singlyLinkedList.length()); // => 4
+singlyLinkedList.remove(3); // remove value
+singlyLinkedList.print(); // => 1 2 4
+singlyLinkedList.remove(9); // remove non existing value
+singlyLinkedList.print(); // => 1 2 4
+singlyLinkedList.remove(1); // remove head
+singlyLinkedList.print(); // => 2 4
+singlyLinkedList.remove(4); // remove tail
+singlyLinkedList.print(); // => 2
+console.log('length is 1:', singlyLinkedList.length()); // => 1
+singlyLinkedList.add(6);
+singlyLinkedList.print(); // => 2 6
+singlyLinkedList.insertAfter(3, 2);
+singlyLinkedList.print(); // => 2 3 6
+singlyLinkedList.insertAfter(4, 3);
+singlyLinkedList.print(); // => 2 3 4 6
+singlyLinkedList.insertAfter(5, 9); // insertAfter a non existing node
+singlyLinkedList.print(); // => 2 3 4 6
+singlyLinkedList.insertAfter(5, 4);
+singlyLinkedList.insertAfter(7, 6); // insertAfter the tail
+singlyLinkedList.print(); // => 2 3 4 5 6 7
+singlyLinkedList.add(8); // add node with normal method
+singlyLinkedList.print(); // => 2 3 4 5 6 7 8
+console.log('length is 7:', singlyLinkedList.length()); // => 7
+singlyLinkedList.traverse(function(node) { node.data = node.data + 10; });
+singlyLinkedList.print(); // => 12 13 14 15 16 17 18
+singlyLinkedList.traverse(function(node) { console.log(node.data); }); // => 12 13 14 15 16 17 18
+console.log('length is 7:', singlyLinkedList.length()); // => 7
