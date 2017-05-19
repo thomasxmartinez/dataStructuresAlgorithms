ac771f7c3be5d679c34962d21283645c02ffc24d
tommytnez <tommytnez@yahoo.com>
2017-05-18 20:13:59 -0700

    yes

--- yes.js
+++ yes.js
@@ -1,36 +1,54 @@
-function longestIncrSubArray(seq) {
-  var longIncArr = [];
-  var initSubArr = [];
-	var start = 0;
-	var end = 0;
-	if (seq < 2) {
-		return [];
-	}
-	for (var i = 0; i < seq.length; i++) {
-		if (seq[i] < (seq[i + 1]) || (i + 1 === seq.length)) {
-			end = end + 1;
-			initSubArr = seq.slice(start, end);
-		}
-		if (seq[i] > (seq[i + 1])) {
-			end = end + 1;
-			initSubArr = seq.slice(start, end);
-			if (longIncArr.length < initSubArr.length) {
-				longIncArr = seq.slice(start, end);
-			}
-			if ((i !== seq.length - 1)) {
-				start = end;
-			}
-		}
-	}
-	if (longIncArr.length < initSubArr.length) {
-		longIncArr = seq.slice(start, end);
-	}
-	if (longIncArr.length > 1) {
-		return longIncArr;
-	} else
-	return [];
+/**
+ * Finds the longest increasing subarray from a given array
+ * of integers. A subarray is said to be "increasing" when each
+ * non-last value is followed by a value that is greater
+ * than the previous value.
+ * If there are multiple contenders of the same length, returns
+ * the first occurring subarray.
+ * If no increasing sub-array of length 2 or greater is found,
+ * returns an empty array.
+ *
+ * @param {number[]} seq  An array of integers
+ * @returns {number[]} The longest increasing subarray of `seq`
+ */
+function longestIncrSubArray(seq){
+  var subArr = [];
+  if (seq.length < 2)
+    return [];
+
+  var maxStart = 0;
+  var maxLength = 1;
+
+  var currentStart = 0;
+  var currentLength = 1;
+
+  for ( var i = 1; i < seq.length; i++) {
+
+    if (seq[i] >= seq[i-1]) {
+      currentLength++;
+      if (i < seq.length - 1) {
+        i++;
+      }
+    }
+
+    if (currentLength > maxLength) {
+      maxLength = currentLength;
+      maxStart = currentStart;
+
+      currentStart = i;
+      currentLength = 0;
+    }
+  }
+
+  if (maxLength > 1) {
+    for (i = maxStart; i < maxStart + maxLength; i++) {
+      subArr.push(seq[i]);
+    }
+  }
+  return subArr;
 }
 
+// Test for the second exercise
 console.log(longestIncrSubArray([3, 4, 2, 8, 3, 9, 11, 9, 10]));
 console.log(longestIncrSubArray([3, 4, 1, 2]));
 console.log(longestIncrSubArray([3, 4, 2, 8]));
